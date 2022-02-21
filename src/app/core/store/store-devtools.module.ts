5f0f6abcf639fba3c3d6eb1e1db274e2fda9241a
Marcel Eisentraut <meisentraut@intershop.de>
2022-02-21 11:27:06 +0100

    feat: remove e2e test and loadProductPriceIfNotLoaded should not be seen in store-devtools

--- src/app/core/store/store-devtools.module.ts
+++ src/app/core/store/store-devtools.module.ts
@@ -5,12 +5,20 @@ import { loadProductIfNotLoaded } from 'ish-core/store/shopping/products';
 import { loadPromotion } from 'ish-core/store/shopping/promotions';
 import { suggestSearch } from 'ish-core/store/shopping/search';
 
+import { loadProductPriceIfNotLoaded } from './shopping/product-prices';
+
 // remove the 'store-devtools.module.ts' fileReplacements in 'angular.json' to enable Store Devtools in production
 
 export const storeDevtoolsModule = [
   StoreDevtoolsModule.instrument({
     maxAge: PRODUCTION_MODE ? 25 : 200,
     logOnly: PRODUCTION_MODE, // restrict extension to log-only mode
-    actionsBlocklist: [loadPromotion.type, loadProductIfNotLoaded.type, setStickyHeader.type, suggestSearch.type],
+    actionsBlocklist: [
+      loadPromotion.type,
+      loadProductIfNotLoaded.type,
+      loadProductPriceIfNotLoaded.type,
+      setStickyHeader.type,
+      suggestSearch.type,
+    ],
   }),
 ];
