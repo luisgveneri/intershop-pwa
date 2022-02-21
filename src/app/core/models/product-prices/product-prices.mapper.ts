b72e27ac7b4636c74cd2c135b28182e55196740e
Marcel Eisentraut <meisentraut@intershop.de>
2022-02-21 15:06:21 +0100

    feature: add scaledPrices to product view

--- src/app/core/models/product-prices/product-prices.mapper.ts
+++ src/app/core/models/product-prices/product-prices.mapper.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { PriceItemMapper } from 'ish-core/models/price-item/price-item.mapper';
-import { PriceItem } from 'ish-core/models/price-item/price-item.model';
+import { PriceItem, ScaledPriceItem } from 'ish-core/models/price-item/price-item.model';
 
 import { ProductPriceDetailsData, ProductPriceItemData } from './product-prices.interface';
 import { ProductPriceDetails } from './product-prices.model';
@@ -17,14 +17,28 @@ function getSinglePrice(prices: ProductPriceItemData[]): PriceItem {
   return;
 }
 
+function getScaledPriceItem(priceItem: ProductPriceItemData): ScaledPriceItem {
+  if (priceItem) {
+    return {
+      ...PriceItemMapper.fromPriceItem(priceItem),
+      minQuantity: priceItem?.minQuantity?.value,
+    };
+  }
+  return;
+}
+
 @Injectable({ providedIn: 'root' })
 export class ProductPricesMapper {
   static fromData(data: ProductPriceDetailsData): ProductPriceDetails {
+    const scaledPrices = data?.prices?.SalePrice?.map(priceItem => getScaledPriceItem(priceItem));
     return {
       sku: data?.sku,
       prices: {
         salePrice: getSinglePrice(data?.prices?.SalePrice) ?? getSinglePrice(data?.prices?.ListPrice),
         listPrice: getSinglePrice(data?.prices?.ListPrice),
+        scaledPrices: !scaledPrices?.find(price => price.minQuantity === 1)
+          ? scaledPrices
+          : scaledPrices.filter(priceItem => priceItem.minQuantity !== 1),
       },
     };
   }
